018a738cbbc208419e35617e17896310c00a5837
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2022-10-19 10:48:55 +0200

    Add .NET Core example (#1)
    * xxx
    
    * readme
    
    * .
    
    * ts
    
    * move

---
+++ core/ReactCoreMvcExample/Program.cs
@@ -0,0 +1,58 @@
+using JavaScriptEngineSwitcher.ChakraCore;
+using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using React.AspNet;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddJsEngineSwitcher(options => options.DefaultEngineName = ChakraCoreJsEngine.EngineName)
+    .AddChakraCore();
+
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddReact();
+
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+// Initialise ReactJS.NET. Must be before static files.
+app.UseReact(config =>
+{
+    config
+        // .SetLoadBabel(false)
+        .AddScriptWithoutTransform("~/js/dist/vendor.js")
+        .AddScriptWithoutTransform("~/js/dist/runtime.js")
+        .AddScriptWithoutTransform("~/js/dist/fluentui.js")
+        .AddScriptWithoutTransform("~/js/dist/components.js")
+        .SetJsonSerializerSettings(new JsonSerializerSettings
+        {
+            StringEscapeHandling = StringEscapeHandling.EscapeHtml
+        })
+        ;
+});
+
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
